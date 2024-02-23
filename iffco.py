ff5429487182ff600c3aec29c2b97436b93768ac
gitprashant <singhprashant0824@gmail.com>
2024-02-23 22:53:39 +0530

    update2

--- iffco.py
+++ iffco.py
@@ -23,7 +23,7 @@ index_name = 'iffco_original_pre'
 os.environ["AZURE_COGNITIVE_SEARCH_SERVICE_NAME"] = "testaicognitivesearch1"
 os.environ["AZURE_COGNITIVE_SEARCH_INDEX_NAME"] = index_name
 os.environ["AZURE_COGNITIVE_SEARCH_API_KEY"] = "6BogobGfe1c6vr67EzV6BgVB2HuIDhwcAboNznRQNlAzSeBE9FFi"
-os.environ["OPENAI_API_KEY"] = "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" 
+os.environ["OPENAI_API_KEY"] = "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" 
 
 
 # Define Azure Cognitive Search as our retriever
